36e18bc154ad36d11ee7eacbb5ecbe2a5cd6a6f6
andrey <andrey.o@list.ru>
2019-02-28 17:35:04 +0400

    Мелкая оптимизация

--- 5/script.js
+++ 5/script.js
@@ -3,13 +3,11 @@ letters = letters.toUpperCase().split('');
 let numbers = '12345678'
 numbers = numbers.split('');
 //Создание массива шахматных фигур в виде HTML кода
-let whiteFigure1 = '&#9817; &#9817; &#9817; &#9817; &#9817; &#9817; &#9817; &#9817;';
+let whiteFigure1 = '&#9817;';
 let whiteFigure2 = '&#9814; &#9816; &#9815; &#9813; &#9812; &#9815; &#9816; &#9814;';
-let blackFigure1 = '&#9823; &#9823; &#9823; &#9823; &#9823; &#9823; &#9823; &#9823;';
+let blackFigure1 = '&#9823;';
 let blackFigure2 = '&#9820; &#9822; &#9821; &#9819; &#9818; &#9821; &#9822; &#9820;';
-whiteFigure1 = whiteFigure1.split(' ');
 whiteFigure2 = whiteFigure2.split(' ');
-blackFigure1 = blackFigure1.split(' ');
 blackFigure2 = blackFigure2.split(' ');
 //Создание массивов ячеек 
 let number1 = $('.number').splice(0, 8);
@@ -27,8 +25,8 @@ for (i=0; i<9; i++) {
     $(word1[i]).html(letters[i]).css({transform: 'rotate(180deg)'});
     $(word2[i]).html(letters[i]);
     $(whiteFigureblock2[i]).html(whiteFigure2[i]);
-    $(whiteFigureblock1[i]).html(whiteFigure1[i]);
-    $(blackFigureblock1[i]).html(blackFigure1[i]);
+    $(whiteFigureblock1[i]).html(whiteFigure1);
+    $(blackFigureblock1[i]).html(blackFigure1);
     $(blackFigureblock2[i]).html(blackFigure2[i]);
 }
 //Заполняю ячейки цветами
