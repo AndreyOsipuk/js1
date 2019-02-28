770c6a7ad0c5219e33d7a9494ac5c76f54f2cbf7
andrey <andrey.o@list.ru>
2019-02-28 05:15:48 +0400

    передалал на гридах, упростил код

--- 5/script.js
+++ 5/script.js
@@ -2,86 +2,56 @@ let letters = 'abcdefgh';
 letters = letters.toUpperCase().split('');
 let numbers = '12345678'
 numbers = numbers.split('');
-
 //Создание массива шахматных фигур в виде HTML кода
-let whiteFigure1 = '&#9817;, &#9817;, &#9817;, &#9817;, &#9817;, &#9817;, &#9817;, &#9817;';
-let whiteFigure2 = '&#9814;, &#9816;, &#9815;, &#9813;, &#9812;, &#9815;, &#9816;, &#9814;';
-let blackFigure1 = '&#9823;, &#9823;, &#9823;, &#9823;, &#9823;, &#9823;, &#9823;, &#9823;';
-let blackFigure2 = '&#9820;, &#9822;, &#9821;, &#9819;, &#9818;, &#9821;, &#9822;, &#9820;';
-whiteFigure1 = whiteFigure1.split(', ');
-whiteFigure2 = whiteFigure2.split(', ');
-blackFigure1 = blackFigure1.split(', ');
-blackFigure2 = blackFigure2.split(', ');
-
-//Заполнение букв и цифр на доске
-let $cell = $('.cell');
-for (i=1; i<9; i++) {
-    $($cell[i]).html(letters[i-1]).css({transform: 'rotate(180deg)'});
-    $($cell[i+10]).html(blackFigure2[i-1]); //Заполнение доски фигурами
-    $($cell[i+20]).html(blackFigure1[i-1]); //Заполнение доски фигурами
+let whiteFigure1 = '&#9817; &#9817; &#9817; &#9817; &#9817; &#9817; &#9817; &#9817;';
+let whiteFigure2 = '&#9814; &#9816; &#9815; &#9813; &#9812; &#9815; &#9816; &#9814;';
+let blackFigure1 = '&#9823; &#9823; &#9823; &#9823; &#9823; &#9823; &#9823; &#9823;';
+let blackFigure2 = '&#9820; &#9822; &#9821; &#9819; &#9818; &#9821; &#9822; &#9820;';
+whiteFigure1 = whiteFigure1.split(' ');
+whiteFigure2 = whiteFigure2.split(' ');
+blackFigure1 = blackFigure1.split(' ');
+blackFigure2 = blackFigure2.split(' ');
+//Создание массивов ячеек 
+let number1 = $('.number').splice(0, 8);
+let number2 = $('.number').splice(8, 8);
+let word1 = $('.word').splice(0, 8);
+let word2 = $('.word').splice(8, 8);
+let whiteFigureblock2 = $('.cell').splice(0, 8);
+let whiteFigureblock1 = $('.cell').splice(8, 8);
+let blackFigureblock1 = $('.cell').splice(48, 8);
+let blackFigureblock2 = $('.cell').splice(56, 8);
+//Заполнение цифр на доске
+for (i=0; i<9; i++) {
+    $(number1[i]).html(numbers[i]);
+    $(number2[i]).html(numbers[i]).css({transform: 'rotate(180deg)'});
+    $(word1[i]).html(letters[i]).css({transform: 'rotate(180deg)'});
+    $(word2[i]).html(letters[i]);
+    $(whiteFigureblock2[i]).html(whiteFigure2[i]);
+    $(whiteFigureblock1[i]).html(whiteFigure1[i]);
+    $(blackFigureblock1[i]).html(blackFigure1[i]);
+    $(blackFigureblock2[i]).html(blackFigure2[i]);
 }
-for (i=91; i<100; i++) {
-    $($cell[i]).html(letters[i-91]);
-    $($cell[i-10]).html(whiteFigure2[i-91]); //Заполнение доски фигурами
-    $($cell[i-20]).html(whiteFigure1[i-91]); //Заполнение доски фигурами
-}
-for (i=10, j=0; i<100; i+=10, j++) {
-    $($cell[i]).html(numbers[j]);
-}
-for (i=19, j=0; i<100; i+=10, j++) {
-    $($cell[i]).html(numbers[j]).css({transform: 'rotate(180deg)'});
-}
-
-//Заполняю черные ячейки
-for (i=12; i<90; i+=2) { 
-    if (i%10 == 0) {
+//Заполняю ячейки цветами
+let check = true;
+for (i=0, j=0; i<64; i+=2) {
+    if (i%8 == 0 && i != 0) {
         i--;
+        $($('.cell')[i+1]).addClass('whiteCell');
         continue;
     }
-    if (String(i)[1] == 9) {
+    if ( (i+1)%8 == 0 ) {
+        $($('.cell')[i]).addClass('blackCell');
         i++;
-        continue;
     }
-    $($cell[i]).addClass('blackCell');
-}
-
-//Заполняю белые ячейки
-for (j=11; j<90; j+=2) {
-    if (j%10 == 0) {
-        j--;
-        continue;
-    }
-    if (String(j)[1] == 9) {
-        j++;
-        continue;
-    }
-    $($cell[j]).addClass('whiteCell');
-}
-
-//Удаляю рамки для крайних ячеек
-for (i=0; i<100; i++) {
-    if (i<10) {
-        $($cell[i]).addClass('outTable');
-    }
-    if (i%10 == 0 && i<91) {
-        $($cell[i]).addClass('outTable');
-    }
-    if (String(i)[1] == 9) {
-        $($cell[i]).addClass('outTable');
-    }
-    if (i>90) {
-        $($cell[i]).addClass('outTable');
-    }
+    $($('.cell')[i]).addClass('blackCell');
+    $($('.cell')[i+1]).addClass('whiteCell');
 }
 let activeCell;
-let beforeCell = $cell[0];
+let beforeCell = $('.cell')[0];
 let click = 0;
 //Логика перемещения фигур
-$($cell).click(function() {
+$($('.cell')).click(function() {
     activeCell = $(this);
-    if (activeCell.hasClass('outTable')) {
-        return;
-    };
     $(beforeCell).removeClass('active');
     if (click == 0) {
         if (activeCell.html() != '') {
@@ -98,18 +68,11 @@ $($cell).click(function() {
     beforeCell = $(this);
     cursor();
 });
-let whiteCell = $('.whiteCell');
-let blackCell = $('.blackCell');
 cursor();
 function cursor() {
-    for (i=0; i<$('.whiteCell').length; i++) {
-        if ($(whiteCell[i]).html() != '') {
-            $(whiteCell[i]).css({cursor: 'pointer'});
-        }
-    }
-    for (i=0; i<$('.blackCell').length; i++) {
-        if ($(blackCell[i]).html() != '') {
-            $(blackCell[i]).css({cursor: 'pointer'});
+    for (i=0; i<$('.cell').length; i++) {
+        if ($($('.cell')[i]).html() != '') {
+            $($('.cell')[i]).css({cursor: 'pointer'});
         }
     }
 }
